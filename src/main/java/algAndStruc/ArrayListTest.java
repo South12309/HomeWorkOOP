a45783592f1a3ce79335f1596cfd45146c49b185
South12309 <south123@mail.ru>
2023-03-20 15:06:02 +0300

    doing ArrayListTest (done add, get, set, find)

--- src/main/java/algAndStruc/ArrayListTest.java
+++ src/main/java/algAndStruc/ArrayListTest.java
@@ -1,7 +1,56 @@
 package algAndStruc;
 
-public class ArrayListTest {
-    Object[] list=new Object[15];
+public class ArrayListTest<E> implements ListTest<E>{
+    private Object[] list;
+    private int size=0;
 
+    public ArrayListTest() {
+        this.list = new Object[15];
+    }
 
+    @Override
+    public boolean add(E e) {
+        if (size>0)
+            size++;
+        list[size]=e;
+        return true;
+    }
+
+    @Override
+    public E get(int i) {
+        chekIndex(i);
+        return (E)list[i];
+    }
+
+    private void chekIndex(int i) {
+        if (i >size)
+            throw new RuntimeException("Индекс больше размера списка");
+    }
+
+    @Override
+    public boolean set(int i, E e) {
+        chekIndex(i);
+        list[i]=e;
+        return true;
+    }
+
+    @Override
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (list[i].equals(e)) {
+                return i;
+            }
+        }
+        throw new RuntimeException("Элемент не найден в списке");
+    }
+
+    @Override
+    public boolean remove(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(int i) {
+        return false;
+    }
 }
--- src/main/java/algAndStruc/LinkedListTest.java
+++ src/main/java/algAndStruc/LinkedListTest.java
@@ -1,6 +1,8 @@
 package algAndStruc;
 
 
+
+
 public class LinkedListTest {
 
 }
