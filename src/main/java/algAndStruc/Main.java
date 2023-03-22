bb062f8ecdf086338b68783151cd050dc8d0e3b4
South12309 <south123@mail.ru>
2023-03-22 14:56:06 +0300

    LinkedListTest done

--- src/main/java/algAndStruc/Main.java
+++ src/main/java/algAndStruc/Main.java
@@ -19,5 +19,25 @@ public class Main {
         System.out.println(list.get(1));
 
         System.out.println("индекс четверки - " + list.find("4"));
+
+        System.out.println("====================================================");
+
+        LinkedListTest<String> listL = new LinkedListTest<>();
+        listL.add("1");
+        listL.add("2");
+        listL.add("3");
+        listL.add("4");
+        listL.add("5");
+        System.out.println(listL);
+
+        listL.set(1, "20");
+        System.out.println(listL);
+
+        listL.remove(1);
+        System.out.println(listL);
+
+        System.out.println(listL.get(1));
+
+        System.out.println("индекс четверки - " + listL.find("4"));
     }
 }
